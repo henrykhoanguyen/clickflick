fecab61934b6813771c5c60564165a43e0bc5430
Khoa Nguyen <henrykhoanguyen@gmail.com>
2019-07-18 13:29:30 -0700

    working on login

---
+++ routes/api/login.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+
+// @route   POST api/login
+// @desc    Get user info and log them in
+// @access   Private
+router.post('/', (req, res) => {
+    
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 const express = require('express');
 const path = require('path');
 const config = require('config');
+const mysql = require('mysql');
+
+// Create connection
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '10Dieuran!',
+    database : 'moviedb'
+});
+
+// Connect
+db.connect((err) => {
+    if(err) throw err;
+
+    console.log("MySQL Connected...");
+});
 
 const app = express();
+
+// Middleware
+app.use(express.json());
+
+// User Routes
+app.use('/api/login', require('./routes/api/login'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
